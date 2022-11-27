956292a4897ba8a7ccfe1748c46fad32e888bbd1
Md Mahbub Rahman <54384173+mahbub099@users.noreply.github.com>
2022-11-27 12:18:19 +0600

    user management app v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+import Users from './components/Users';
+
+const App = () => {
+  // step1 : declare three states here : users, isLoading, error
+    const [users, setUsers]=useState([]);
+    const [isLoading, setIsLoading]=useState(true);
+    const [error, setError]=useState(null);
+
+  // step2 : use useEffect for fetching the data including updating isLoading and error states
+  useEffect(() => {
+    fetch( "https://jsonplaceholder.typicode.com/users")
+    .then((res)=>{
+      if (!res.ok) {
+        throw Error('User is Not Availble');
+      }
+      else{
+        return res.json();
+      }
+    })
+    .then((users)=>{
+      setUsers(users);
+      setIsLoading(false);
+      setError(null);
+    })
+    .catch((error)=>{
+      setError(error.message);
+      setIsLoading(false);
+    })
+  },[]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Users Management App</h1>
+      {isLoading && <p>Loading users...</p>}
+      {error && <p>{error.message}</p>}
+      {users && <Users users={users} />}
     </div>
   );
-}
+};
 
 export default App;
+
